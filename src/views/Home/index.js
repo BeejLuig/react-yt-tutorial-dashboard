e6d77168ed2ffd2cc05fac7e256d5380c8dcc1ed
BeejLuig <bjcantlupe@gmail.com>
2017-04-22 23:47:10 -0400

    Add some content to Home component

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Home = () => <div>Home</div>
+const Home = () => (
+  <section className="hero is-primary">
+    <div className="hero-body">
+      <h1 className="title">YouTube Tutorial Dashboard</h1>
+      <div className="subtitle">
+        <p>Import your favorite playlists</p>
+        <p>Track your progress</p>
+      </div>
+    </div>
+    <footer className="hero-footer">
+      <div className="container">
+        <div className="content has-text-centered">
+          <p>
+            <Link to="/signup" className="button is-success is-inverted is-medium is-outlined">Try it now</Link>
+          </p>
+          <br />
+        </div>
+      </div>
+    </footer>
+  </section>
+)
 
 export default Home;
