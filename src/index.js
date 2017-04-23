9c4d82576d0b31d357782124144f82cd70c5f42d
BeejLuig <bjcantlupe@gmail.com>
2017-04-23 13:50:36 -0400

    move createStore method into src/index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+// node module imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+
+// custom module imports
 import './index.css';
 import './bulma-docs.css';
+import App from './containers/App';
+import rootReducer from './redux/store'
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App store={store} />
+  </Provider>,
   document.getElementById('root')
 );
