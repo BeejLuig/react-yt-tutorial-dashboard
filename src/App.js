a5e71713e49855bda37984bf3f09df7c4b75a1c7
BeejLuig <bjcantlupe@gmail.com>
2017-04-22 22:13:34 -0400

    Add react-dom and react-router-dom. Add initial routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom';
+
+const Navbar = () => (
+  <nav className="nav has-shadow has-text-centered">
+
+    <div className="nav-left">
+      <Link to="/" className="nav-item">YT Tutorial Dashboard</Link>
+    </div>
+
+    <div className="nav-right nav-menu is-active">
+      <Link to="/signup" className="nav-item">Sign up</Link>
+      <Link to="/login" className="nav-item" >Log in</Link>
+    </div>
+  </nav>
+);
+
+const Signup = () => <div>Signup</div>
+const Home = () => <div>Home</div>
+const Login = () => <div>Login</div>
+const NotFound = () => <div>Not Found</div>
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <nav className="nav has-shadow">
-
-          <div className="nav-left is-active">
-          <a className="nav-item">
-            Home
-          </a>
-          </div>
-
-          <div className="nav-right nav-menu is-active">
-            <a className="nav-item">
-              Sign up
-            </a>
-            <a className="nav-item">
-              Log in
-            </a>
-          </div>
-        </nav>
-      </div>
+      <Router>
+        <div className="App">
+        <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
