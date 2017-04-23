8dc87de3c9e4e28180780840d2d16f3666d266f8
BeejLuig <bjcantlupe@gmail.com>
2017-04-23 13:49:26 -0400

    refactor Signup form to work with reduxForm

--- src/views/Signup/index.js
+++ src/views/Signup/index.js
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const Signup = () => (
-  <section className="section">
-    <div className="container">
-      <div>
-        <h1 className="title">Sign Up</h1>
-      </div>
-      <br />
-      <form>
-        <div className="field">
-          <label className="label">Username</label>
-          <p className="control">
-            <input className="input" type="text" placeholder="Username"/>
-          </p>
-        </div>
-        <div className="field">
-          <label className="label">Password</label>
-          <p className="control">
-            <input className="input" type="password" placeholder="Password" />
-          </p>
-        </div>
-        <div className="field">
-          <p className="control">
-            <button className="button is-success">
-              Sign up
-            </button>
-          </p>
-        </div>
-      </form>
-    </div>
-  </section>
-)
 
-export default Signup;
+class Signup extends Component {
+
+  render() {
+
+    const { pristine, submitting, handleSubmit } = this.props;
+
+    return (
+      <section className="section">
+        <div className="container">
+          <div>
+            <h1 className="title">Sign Up</h1>
+          </div>
+          <br />
+          <form onSubmit={handleSubmit}>
+            <div className="field">
+              <label className="label" htmlFor="username">Username</label>
+              <p className="control">
+                <Field name="username" className="input" component="input" type="text" placeholder="Username"/>
+              </p>
+            </div>
+            <div className="field">
+              <label className="label" htmlFor="password">Password</label>
+              <p className="control">
+                <Field name="password" className="input" component="input" type="password" placeholder="Password" />
+              </p>
+            </div>
+            <div className="field">
+              <p className="control">
+                <button type="submit" className="button is-success" disabled={pristine || submitting}>Sign up</button>
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default reduxForm({
+  form: 'signup'  // a unique identifier for this form
+})(Signup)
