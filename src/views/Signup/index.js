f1d53777c94a677828adfaca850ac5ffc9717efd
BeejLuig <bjcantlupe@gmail.com>
2017-04-22 23:11:32 -0400

    Build out form in Signup component

--- src/views/Signup/index.js
+++ src/views/Signup/index.js
@@ -1,5 +1,34 @@
 import React from 'react';
 
-const Signup = () => <div>Signup</div>
+const Signup = () => (
+  <div className="container column">
+    <br /> 
+    <div>
+      <h1 className="title">Sign Up</h1>
+    </div>
+    <br />
+    <form>
+      <div className="field">
+        <label class="label">Username</label>
+        <p className="control">
+          <input className="input" type="text" placeholder="Username"/>
+        </p>
+      </div>
+      <div className="field">
+        <label class="label">Password</label>
+        <p className="control">
+          <input className="input" type="password" placeholder="Password" />
+        </p>
+      </div>
+      <div className="field">
+        <p className="control">
+          <button className="button is-success">
+            Login
+          </button>
+        </p>
+      </div>
+    </form>
+  </div>
+)
 
 export default Signup;
