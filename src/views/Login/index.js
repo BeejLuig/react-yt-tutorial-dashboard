f1d042a3d97da19dde484b46bbfe96c21417423b
BeejLuig <bjcantlupe@gmail.com>
2017-04-22 23:46:54 -0400

    Add login form to Login component

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,5 +1,35 @@
 import React from 'react';
 
-const Login = () => <div>Login</div>
+const Login = () => (
+  <section className="section">
+    <div className="container">
+      <div>
+        <h1 className="title">Login</h1>
+      </div>
+      <br />
+      <form>
+        <div className="field">
+          <label class="label">Username</label>
+          <p className="control">
+            <input className="input" type="text" placeholder="Username"/>
+          </p>
+        </div>
+        <div className="field">
+          <label class="label">Password</label>
+          <p className="control">
+            <input className="input" type="password" placeholder="Password" />
+          </p>
+        </div>
+        <div className="field">
+          <p className="control">
+            <button className="button is-success">
+              Login
+            </button>
+          </p>
+        </div>
+      </form>
+    </div>
+  </section>
+)
 
 export default Login;
