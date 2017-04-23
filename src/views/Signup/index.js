176ad17a76544594540ff82db310bd57271ef812
BeejLuig <bjcantlupe@gmail.com>
2017-04-22 23:46:20 -0400

    Add sign up form to Signup component

--- src/views/Signup/index.js
+++ src/views/Signup/index.js
@@ -1,34 +1,35 @@
 import React from 'react';
 
 const Signup = () => (
-  <div className="container column">
-    <br /> 
-    <div>
-      <h1 className="title">Sign Up</h1>
+  <section className="section">
+    <div className="container">
+      <div>
+        <h1 className="title">Sign Up</h1>
+      </div>
+      <br />
+      <form>
+        <div className="field">
+          <label class="label">Username</label>
+          <p className="control">
+            <input className="input" type="text" placeholder="Username"/>
+          </p>
+        </div>
+        <div className="field">
+          <label class="label">Password</label>
+          <p className="control">
+            <input className="input" type="password" placeholder="Password" />
+          </p>
+        </div>
+        <div className="field">
+          <p className="control">
+            <button className="button is-success">
+              Sign up
+            </button>
+          </p>
+        </div>
+      </form>
     </div>
-    <br />
-    <form>
-      <div className="field">
-        <label class="label">Username</label>
-        <p className="control">
-          <input className="input" type="text" placeholder="Username"/>
-        </p>
-      </div>
-      <div className="field">
-        <label class="label">Password</label>
-        <p className="control">
-          <input className="input" type="password" placeholder="Password" />
-        </p>
-      </div>
-      <div className="field">
-        <p className="control">
-          <button className="button is-success">
-            Login
-          </button>
-        </p>
-      </div>
-    </form>
-  </div>
+  </section>
 )
 
 export default Signup;
