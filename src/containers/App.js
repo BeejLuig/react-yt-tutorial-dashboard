1bcc6a483dfd2b2e193da9b824cbf82a122fec5b
BeejLuig <bjcantlupe@gmail.com>
2017-04-23 13:50:55 -0400

    pass onSubmit handler to Signup component from App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,14 +5,22 @@ import {
   Switch
 } from 'react-router-dom';
 
-// local imports
+// custom node modules
 import Home from '../views/Home';
 import Login from '../views/Login';
 import Signup from '../views/Signup';
 import Navbar from '../views/Navbar';
 import NotFound from '../views/NotFound'
+import { signup } from '../redux/modules/Auth/actions';
 
 class App extends Component {
+
+  handleSignin(values) {
+    this.props.store.dispatch(
+      signup(values)
+    );
+  }
+
   render() {
     return (
       <Router>
@@ -20,7 +28,9 @@ class App extends Component {
         <Navbar />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/signup">
+              <Signup onSubmit={this.handleSignin.bind(this)} />
+            </Route>
             <Route exact path="/login" component={Login} />
             <Route component={NotFound} />
           </Switch>
