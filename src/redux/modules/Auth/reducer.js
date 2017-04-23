e9633ad10ba1747b2bd3a623ff94ab62eaaae668
BeejLuig <bjcantlupe@gmail.com>
2017-04-23 13:50:00 -0400

    create initial Auth reducer

---
+++ src/redux/modules/Auth/reducer.js
@@ -0,0 +1,29 @@
+const initialState = {
+  isAuthenticated: false,
+  isAuthenticating: true,
+  currentUser: {
+    id: "",
+    username: ""
+  }
+}
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+
+    case 'AUTHENTICATION_REQUEST':
+      return {
+        ...state,
+        isAuthenticating: true
+      };
+
+    case 'AUTHENTICATION_SUCCESS':
+      return {
+        isAuthenticated: true,
+        isAuthenticating: false,
+        currentUser: action.user
+      };
+
+    default:
+      return state;
+  }
+}
