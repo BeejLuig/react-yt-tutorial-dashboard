7012cffb0a9caba283e73243e2814e50ba034575
BeejLuig <bjcantlupe@gmail.com>
2017-04-22 23:46:41 -0400

    Clean up some Navbar styling

--- src/views/Navbar/index.js
+++ src/views/Navbar/index.js
@@ -3,20 +3,21 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <nav className="nav has-shadow has-text-centered">
+    <div className="container">
+      <div className="nav-left">
+        <Link to="/" className="nav-item">YT Tutorial Dashboard</Link>
+      </div>
 
-    <div className="nav-left">
-      <Link to="/" className="nav-item">YT Tutorial Dashboard</Link>
-    </div>
+      <span className="nav-toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
 
-    <span className="nav-toggle">
-      <span></span>
-      <span></span>
-      <span></span>
-    </span>
-
-    <div className="nav-right nav-menu is-active is-hidden-touch">
-      <Link to="/signup" className="nav-item">Sign up</Link>
-      <Link to="/login" className="nav-item" >Log in</Link>
+      <div className="nav-right nav-menu is-active is-hidden-touch">
+        <Link to="/signup" className="nav-item">Sign up</Link>
+        <Link to="/login" className="nav-item" >Log in</Link>
+      </div>
     </div>
   </nav>
 );
