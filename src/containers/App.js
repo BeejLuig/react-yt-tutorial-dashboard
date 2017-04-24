f98eb6b56228e1857a38b0f224974430265668a0
BeejLuig <bjcantlupe@gmail.com>
2017-04-23 21:14:23 -0400

    add authentication logic to App component

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,34 +4,55 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 // custom node modules
 import Home from '../views/Home';
 import Login from '../views/Login';
 import Signup from '../views/Signup';
 import Navbar from '../views/Navbar';
-import NotFound from '../views/NotFound'
-import { signup } from '../redux/modules/Auth/actions';
+import NotFound from '../views/NotFound';
+import Dashboard from '../views/Dashboard/';
+import MatchAuthenticated from '../components/MatchAuthenticated/';
+import RedirectUnauthenticated from '../components/RedirectUnauthenticated/';
+import { authenticate, authenticationFailure, logout } from '../redux/modules/Auth/actions';
+
+type Props = {
+  isAuthenticating: boolean,
+  isAuthenticated: boolean,
+  logout: () => void,
+  authenticate: () => void,
+  authenticationFailure: () => void
+}
 
 class App extends Component {
 
-  handleSignin(values) {
-    this.props.store.dispatch(
-      signup(values)
-    );
+  props: Props
+
+  componentDidMount() {
+    const token: string = localStorage.getItem('token');
+    if (token) {
+      console.log('Fetching a new token!');
+      this.props.authenticate();
+    } else {
+      this.props.authenticationFailure();
+    }
   }
 
   render() {
+
+    const { isAuthenticated, isAuthenticating, currentUser, logout } = this.props;
+    const authProps = { isAuthenticated, isAuthenticating, currentUser };
+
     return (
       <Router>
         <div className="App">
-        <Navbar />
+        <Navbar isAuthenticated={isAuthenticated}  logout={logout} />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/signup">
-              <Signup onSubmit={this.handleSignin.bind(this)} />
-            </Route>
-            <Route exact path="/login" component={Login} />
+            <MatchAuthenticated path="/dashboard" exact component={Dashboard} {...authProps} />
+            <RedirectUnauthenticated exact path="/login" component={Login} {...authProps} />
+            <RedirectUnauthenticated exact path="/signup" component={Signup} {...authProps} />
             <Route component={NotFound} />
           </Switch>
         </div>
@@ -40,4 +61,10 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    isAuthenticating: state.auth.isAuthenticating,
+    isAuthenticated: state.auth.isAuthenticated,
+    currentUser: state.auth.currentUser
+  }), { logout, authenticate, authenticationFailure }
+)(App);
