ef49782ffcb5cd547decb8150e697d73a736d32e
BeejLuig <bjcantlupe@gmail.com>
2017-04-23 21:14:00 -0400

    clean up index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
-// node module imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk'
+import { Provider } from 'react-redux';
 
-// custom module imports
-import './index.css';
-import './bulma-docs.css';
+// import custom modules
+import store from './redux/store';
 import App from './containers/App';
-import rootReducer from './redux/store'
-
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk)
-);
+import './bulma-docs.css'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App store={store} />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
