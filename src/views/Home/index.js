e9d37b30f2ecf312ff3ac979a94f81e7c697c575
BeejLuig <bjcantlupe@gmail.com>
2017-04-23 21:11:33 -0400

    add isAuthenticated logic to "call to action" button

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,32 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Home = () => (
-  <section className="hero is-primary">
-    <div className="hero-body">
-      <div className="container">
-        <div className="columns is-vcentered">
-          <div className="column">
-            <p className="title">YouTube Tutorial Dashboard</p>
-            <div className="subtitle">
-              <p>Import your favorite playlists</p>
-              <p>Track your progress</p>
+type Props = {
+  isAuthenticated: boolean,
+}
+
+class Home extends Component {
+
+  props: Props
+
+  render() {
+
+    const { isAuthenticated } = this.props;
+    console.log(isAuthenticated)
+    return (
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-vcentered">
+              <div className="column">
+                <p className="title">YouTube Tutorial Dashboard</p>
+                <div className="subtitle">
+                  <p>Import your favorite playlists</p>
+                  <p>Track your progress</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-    <footer className="hero-footer">
-      <div className="container">
-        <div className="content has-text-centered">
-          <p>
-            <Link to="/signup" className="button is-success is-inverted is-medium is-outlined">Try it now</Link>
-          </p>
-          <br />
-        </div>
-      </div>
-    </footer>
-  </section>
-)
+        <footer className="hero-footer">
+          <div className="container">
+            <div className="content has-text-centered">
+              <p>
+                <Link to={isAuthenticated ? "/dashboard" : "/signup"} className="button is-success is-inverted is-medium is-outlined">Get Started</Link>
+              </p>
+              <br />
+            </div>
+          </div>
+        </footer>
+      </section>
+    )
+  }
+}
 
-export default Home;
+export default connect(
+  state => ({
+    isAuthenticating: state.auth.isAuthenticating,
+    isAuthenticated: state.auth.isAuthenticated,
+  })
+)(Home);
