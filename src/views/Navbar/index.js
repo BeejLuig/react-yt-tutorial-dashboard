641530052831e71fa912721b8bb98fab6b1f01b6
BeejLuig <bjcantlupe@gmail.com>
2017-04-23 21:09:56 -0400

    Refactor Navbar to deal with authenticated user

--- src/views/Navbar/index.js
+++ src/views/Navbar/index.js
@@ -1,30 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const Navbar = () => (
-  <nav className="nav has-shadow has-text-centered">
-    <div className="container">
-      <div className="nav-left">
-        <Link to="/" className="nav-item">YTTD</Link>
-      </div>
+type Props = {
+  isAuthenticated: boolean,
+  logout: () => void,
+}
 
-      <span className="nav-toggle"
-        onClick={(e)=>{
-            e.target.classList.toggle('is-active');
-            document.getElementById("menu").classList.toggle('is-active');
+class Navbar extends Component {
+
+  static contextTypes = {
+    router: PropTypes.object,
+  }
+
+  props: Props
+
+  handleLogout = () => this.props.logout(this.context.router);
+
+  render() {
+
+    const { isAuthenticated } = this.props;
+    return (
+      <nav className="nav has-shadow has-text-centered">
+        <div className="container">
+          <div className="nav-left">
+            <NavLink to="/" className="nav-item">YTTD</NavLink>
+          </div>
+
+          <span className="nav-toggle"
+            onClick={(e)=>{
+                e.target.classList.toggle('is-active');
+                document.getElementById("menu").classList.toggle('is-active');
+              }
+            }>
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+
+          { isAuthenticated ?
+            <div id="menu" className="nav-right nav-menu">
+                <NavLink
+                  to="/dashboard"
+                  className="nav-item"
+                >Dashboard</NavLink>
+                <NavLink
+                  to="/"
+                  className="nav-item"
+                  onClick={this.handleLogout}
+                >Logout</NavLink>
+            </div>
+
+            :
+
+            <div id="menu" className="nav-right nav-menu">
+                <NavLink
+                  to="/signup"
+                  className="nav-item"
+                >Sign up</NavLink>
+                <NavLink
+                  to="/login"
+                  className="nav-item"
+                >Log in</NavLink>
+            </div>
           }
-        }>
-        <span></span>
-        <span></span>
-        <span></span>
-      </span>
+        </div>
+      </nav>
+    )
+  }
+}
 
-      <div id="menu" className="nav-right nav-menu">
-        <Link to="/signup" className="nav-item">Sign up</Link>
-        <Link to="/login" className="nav-item" >Log in</Link>
-      </div>
-    </div>
-  </nav>
-);
-
-export default Navbar
+export default Navbar;
