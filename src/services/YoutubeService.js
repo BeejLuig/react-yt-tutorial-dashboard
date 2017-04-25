e5e1a5e797f0ec538f2d2deed99302d0af8746f8
BeejLuig <bjcantlupe@gmail.com>
2017-04-24 21:49:31 -0400

    Add initial YoutubeService actions

---
+++ src/services/YoutubeService.js
@@ -0,0 +1,47 @@
+import { API_KEY } from '../config/env.js';
+
+export function getPlaylistVideos(playlistId) {
+
+  const GET_URL = "https://www.googleapis.com/youtube/v3/playlistItems"
+
+  let requestUrl = `${GET_URL}?key=${API_KEY}&part=snippet&playlistId=${playlistId}&maxResults=50`;
+  const headers = new Headers({
+    'Content-Type': 'application/json'
+  })
+
+  const init = {
+    method: 'GET',
+    headers: headers,
+    cache: 'default',
+    origin: 'http://localhost:3000'
+  }
+
+  return fetch(requestUrl, init)
+    .then(resp => resp.json()
+      .then(json => json)
+  );
+
+}
+
+export function getPlaylists(playlistId) {
+
+  const GET_URL = "https://www.googleapis.com/youtube/v3/playlists"
+
+  let requestUrl = `${GET_URL}?key=${API_KEY}&part=snippet&id=${playlistId}&maxResults=50`;
+  const headers = new Headers({
+    'Content-Type': 'application/json'
+  })
+
+  const init = {
+    method: 'GET',
+    headers: headers,
+    cache: 'default',
+    origin: 'http://localhost:3000'
+  }
+
+  return fetch(requestUrl, init)
+    .then(resp => resp.json()
+      .then(json => json)
+  );
+
+}
