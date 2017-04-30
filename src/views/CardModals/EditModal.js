a9c005855268b91f46a9794608a8d8db4e2a4ee3
BeejLuig <bjcantlupe@gmail.com>
2017-04-30 16:02:01 -0400

    Add EditModal component

--- src/views/CardModals/EditModal.js
+++ src/views/CardModals/EditModal.js
@@ -1,21 +1,36 @@
 import React from 'react';
 
 const EditModal = (props) => {
-  const { playlist, onClick, handleDelete } = props
+  const { playlist, onClick, handleDelete, handleRefresh, handleReset } = props
   const { title } = playlist
   return (
     <div className="modal is-active">
       <div className="modal-background" onClick={onClick}></div>
         <div id="modal-content" className="modal-content">
           <div className="columns">
-            <div className="column is-half is-offset-one-quarter">
-              <p className="title">Edit Playlist: <b>{title}</b></p>
-              <p>Refresh playlist to match edits made by publishing channel or delete playlist from your account (can't be undone!)</p>
+            <div className="column is-10 is-offset-1">
+              <p className="title">Edit Playlist: <br /><b>{title}</b></p>
+              <ul>
+                <li>
+                  <p><b>Refresh</b> playlist to match edits made by publishing channel</p>
+                </li>
+                <li>
+                  <p><b>Reset</b> all videos in playlist to incomplete status</p>
+                </li>
+                <li>
+                  <p><b>Delete</b> delete playlist from your account (can't be undone!)</p>
+                </li>
+              </ul>
               <br />
               <div className="field is-grouped">
                 <p className="control">
-                  <a className="button is-info">
+                  <a className="button is-info" onClick={handleRefresh}>
                     Refresh
+                  </a>
+                </p>
+                <p className="control">
+                  <a className="button is-primary" onClick={handleReset}>
+                    Reset
                   </a>
                 </p>
                 <p className="control">
