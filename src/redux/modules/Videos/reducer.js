d945cea1c30c158552e21c889448459e2ec423a3
BeejLuig <bjcantlupe@gmail.com>
2017-04-30 16:07:17 -0400

    Update Video reducer and add a couple cases
    SET_ACTIVE_VIDEO
    COMPLETE_VIDEO
    RESET_VIDEOS

--- src/redux/modules/Videos/reducer.js
+++ src/redux/modules/Videos/reducer.js
@@ -1,27 +1,69 @@
 const initialState = {
-  videos: [
-    {
-      title: "",
-      video_id: "",
-      description: "",
-      thumbnail_url: "",
-      playlist_id: "",
-      complete?: false
-    }
-  ]
+  isRequesting: false,
+  errors: [],
+  videos: [],
+  activeVideo: {}
 }
 
 export default (state = initialState, action) => {
+  let videoIndex;
+
   switch(action.type) {
 
     case 'PLAYLIST_VIDEOS_REQUEST':
-      return {}
+      return {
+        ...state,
+        isRequesting: true
+      }
 
     case 'PLAYLIST_VIDEOS_SUCCESS':
-      return {}
+      return {
+        ...state,
+        isRequesting: false,
+        videos: action.videos
+      }
 
     case 'PLAYLIST_VIDEOS_FAILURE':
-      return {};
+      return {
+        ...state,
+        isRequesting: false,
+        errors: action.errors
+      };
+
+    case 'SET_ACTIVE_VIDEO':
+      videoIndex = state.videos.findIndex(video => video.id === action.activeVideo.id)
+      const cleanedVideos = state.videos.map(video => {
+        return { ...video, is_active: false }
+      });
+      return {
+        ...state,
+        isRequesting: false,
+        activeVideo: action.activeVideo,
+        videos: [
+          ...cleanedVideos.slice(0, videoIndex),
+          action.activeVideo,
+          ...cleanedVideos.slice(videoIndex + 1)
+        ]
+      }
+
+    case 'COMPLETE_VIDEO':
+      videoIndex = state.videos.findIndex(video => video.id === action.video.id)
+      return {
+        ...state,
+        isRequesting: false,
+        videos: [
+          ...state.videos.slice(0, videoIndex),
+          action.video,
+          ...state.videos.slice(videoIndex + 1)
+        ]
+      }
+
+    case 'RESET_VIDEOS':
+      return {
+        ...state,
+        isRequesting: false,
+        videos: [...action.videos]
+      }
 
     default:
       return state;
