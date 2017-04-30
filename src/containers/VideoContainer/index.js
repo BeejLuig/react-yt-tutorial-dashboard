55f63ce8ac488a18bcad88c37eeb0d61db4acf3a
BeejLuig <bjcantlupe@gmail.com>
2017-04-30 16:05:17 -0400

    Move components out of VideoContainer, add event handlers

--- src/containers/VideoContainer/index.js
+++ src/containers/VideoContainer/index.js
@@ -1,102 +1,67 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './VideoContainer.css'
-import YouTube from 'react-youtube';
+import Video from '../../views/Video/';
+import PlaylistPanel from '../../views/PlaylistPanel/';
+import { getPlaylistVideos, switchActiveVideos, completeVideo } from '../../redux/modules/Videos/actions';
+import { getUserPlaylists } from '../../redux/modules/Playlists/actions';
 
 // custom node modules
 
-type Props = {
-
-}
-
 class VideoContainer extends Component {
 
-  props: Props
+  static contextTypes = {
+    router: PropTypes.object,
+  }
 
   componentDidMount() {
+    const pathname = this.props.location.pathname.split("/");
+    const id = pathname[pathname.length - 1];
+    this.props.getPlaylistVideos(id);
+    this.props.getUserPlaylists();
+  }
+
+  getId(target){
+    if(target.dataset.id) {
+      return parseInt(target.dataset.id, 10)
+    } else {
+      return this.getId(target.parentNode)
+    }
+  }
+
+  handleClick(e){
+    const newId = this.getId(e.target);
+    this.props.switchActiveVideos(newId)
+  }
+
+  handleComplete() {
+    const id = this.props.activeVideo.id;
+    this.props.completeVideo(id);
   }
 
   render() {
 
-    const options = {
-      width: "100%",
-      maxWidth: "640",
-      maxHeight: "360"
-    }
+    const { videos, playlists, activeVideo, playlistIsRequesting, videoIsRequesting } = this.props;
+    const playlist = playlists.find(playlist => playlist.id === activeVideo.playlist_id)
 
     return (
       <section className="section">
         <div className="columns">
           <div className="column">
-            <div className="container is-fluid">
-              <header className="header" id="video-header">
-                <h1 className="title is-spaced">Install Ruby on Rails on Windows 7, 8 or 10 in 3 Minutes</h1>
-                <h2 className="subtitle">
-                  Tutorials: video 1
-                  <span className="pull-right">Codecademy</span>
-                </h2>
-              </header>
-              <div className="has-text-centered">
-                <section id="video-container" className="content has-text-centered">
-                  <YouTube
-                    videoId="OHgXELONyTQ"
-                    opts={options}
-                    className="is-clearfix is-centered-vertically"
-                  />
-                </section>
-              </div>
-              <footer id="video-footer">
-                <div className="section">
-                  <p className="header"><b>Description</b></p>
-                  <p className="content">
-                    We'll walk you through how to set up Ruby on Rails on your Windows machine in less than 5 minutes. We'll be using http://railsinstaller.org/en to make the installation easier.
-                  </p>
-                </div>
-                <hr />
-              </footer>
-            </div>
+
+        { playlistIsRequesting || videoIsRequesting ? null : <Video video={activeVideo} playlist={playlist} /> }
+
           </div>
           <div className="column is-one-third">
-            <aside id="video-list">
-              <div className="panel">
-                <p className="panel-heading">
-                  Videos
-                </p>
-                <div className="panel-block">
-                  <p className="control">
-                    <a className="button is-primary is-outlined is-fullwidth">Done with this video</a>
-                  </p>
-                </div>
-                <a className="panel-block">
-                  <span className="panel-icon">
-                    <i className="fa fa-circle done" aria-hidden="true"></i>
-                  </span>
-                  <span className="panel-text">Video1</span>
-                </a>
-
-                <a className="panel-block">
-                  <span className="panel-icon">
-                    <i className="fa fa-circle" aria-hidden="true"></i>
-                  </span>
-                  <span className="panel-text">Video1</span>
-                </a>
-
-                <a className="panel-block">
-                  <span className="panel-icon">
-                    <i className="fa fa-circle" aria-hidden="true"></i>
-                  </span>
-                  <span className="panel-text">Video1</span>
-                </a>
-
-                <a className="panel-block">
-                  <span className="panel-icon">
-                    <i className="fa fa-circle" aria-hidden="true"></i>
-                  </span>
-                  <span className="panel-text">Video1</span>
-                </a>
-              </div>
-            </aside>
+          { playlistIsRequesting || videoIsRequesting ? null
+            :
+            <PlaylistPanel
+              onClick={this.handleClick.bind(this)}
+              videoIsRequesting={videoIsRequesting}
+              handleComplete={this.handleComplete.bind(this)}
+            />
+          }
           </div>
         </div>
       </section>
@@ -105,5 +70,11 @@ class VideoContainer extends Component {
 }
 
 export default connect(
-  state => ({}), { }
+  state => ({
+    playlists: state.playlists.playlists,
+    videos: state.videos.videos,
+    activeVideo: state.videos.activeVideo,
+    playlistIsRequesting: state.playlists.isRequesting,
+    videoIsRequesting: state.playlists.isRequesting
+  }), { getPlaylistVideos, getUserPlaylists, switchActiveVideos, completeVideo }
 )(VideoContainer);
