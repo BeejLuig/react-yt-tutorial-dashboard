075c7e72ee37ee7703f60b51028a0980ecc85bed
BeejLuig <bjcantlupe@gmail.com>
2017-04-30 16:06:24 -0400

    Video redux action creators and actions

---
+++ src/redux/modules/Videos/actions.js
@@ -0,0 +1,118 @@
+import ApiService from '../../../services/Api';
+import { resetPlaylistVideos } from '../Playlists/actions'
+// action creators
+
+export const playlistVideosRequest = () => {
+  return {
+    type: 'PLAYLIST_VIDEOS_REQUEST'
+  }
+}
+
+export const playlistVideosSuccess = (videos) => {
+  return {
+    type: 'PLAYLIST_VIDEOS_SUCCESS',
+    videos
+  }
+}
+
+export const playlistVideosFailure = (errors) => {
+  return {
+    type: 'PLAYLIST_VIDEOS_FAILURE',
+    errors
+  }
+}
+
+export const setActiveVideo = (activeVideo) => {
+  return {
+    type: 'SET_ACTIVE_VIDEO',
+    activeVideo
+  }
+}
+
+export const completeActiveVideo = (video) => {
+  return {
+    type: 'COMPLETE_VIDEO',
+    video
+  }
+}
+
+export const resetVideos = (videos) => {
+  return {
+    type: 'RESET_VIDEOS',
+    videos
+  }
+}
+
+// async actions
+
+export const getPlaylistVideos = (id) => {
+  return dispatch => {
+    dispatch(playlistVideosRequest());
+    return ApiService.get(`/playlists/${id}/videos`)
+      .then(videos => {
+        dispatch(playlistVideosSuccess(videos));
+        let activeVideo = videos.find(video => video.is_active);
+        if (activeVideo) {
+          dispatch(setActiveVideo(activeVideo));
+        } else {
+          return ApiService.post(`/videos/${videos[0].id}/activate`)
+            .then(activeVideo => {
+              dispatch(setActiveVideo(activeVideo));
+            })
+        }
+      })
+      .catch(errors => {
+        console.log(errors)
+        dispatch(playlistVideosFailure(errors))
+      });
+  }
+}
+
+export const switchActiveVideos = (newId) => {
+  return dispatch => {
+    dispatch(playlistVideosRequest());
+    return ApiService.post(`/videos/${newId}/activate`)
+    .then(video => {
+      dispatch(setActiveVideo(video))
+    })
+    .catch(errors => {
+      console.log(errors)
+      dispatch(playlistVideosFailure(errors))
+    });
+  }
+}
+
+export const completeVideo = (id) => {
+  return (dispatch, getState) => {
+    dispatch(playlistVideosRequest());
+    ApiService.post(`/videos/${id}/complete`)
+    .then(video => {
+      dispatch(completeActiveVideo(video));
+      const nextVideoId = video.id + 1;
+      return ApiService.post(`/videos/${nextVideoId}/activate`)
+      .then(video => {
+        dispatch(setActiveVideo(video))
+      })
+    })
+    .catch(errors => {
+      console.log(errors);
+      dispatch(playlistVideosFailure(errors));
+    });
+  }
+}
+
+export const resetCompletedVideos = (playlistId) => {
+  return dispatch => {
+    dispatch(playlistVideosRequest());
+    ApiService.post(`/playlists/${playlistId}/reset_videos`)
+      .then(videos => {
+        dispatch(resetVideos(videos));
+        dispatch(resetPlaylistVideos(playlistId));
+        dispatch(switchActiveVideos(videos[0].id));
+        alert("Videos successfully reset");
+      })
+      .catch(errors => {
+        console.log(errors);
+      });
+  }
+}
